ff20f1956c4a4da3a8e7ad0a7b6c63e44ca0ca33
SanoKiyo <sano-k@ichirou.co.jp>
2022-01-30 23:36:18 +0900

    udemy学習 TSのEnumなどについて

--- udemy_understanding_typescript/app.js
+++ udemy_understanding_typescript/app.js
@@ -1,12 +1,35 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    return result;
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+// Tupleはpushは許可されてしまうことに注意
+// person.role.push('admin');
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'yota',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+var favariteActivities;
+favariteActivities = ['Sports'];
+console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var pritnResult = true;
-var resultPhrase = 'Result: ';
-add(number1, number2, pritnResult, resultPhrase);
+if (person.role === Role.ADMIN) {
+    console.log('ADMIN!');
+}
