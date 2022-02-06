9a6ddaae6e9acc8f0973b579bfb39f36e9fe8864
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2022-02-06 21:40:09 +0900

    udemy学習 TSの高度な型についての学習

--- udemy_understanding_typescript/dist/app.js
+++ udemy_understanding_typescript/dist/app.js
@@ -1,13 +1,66 @@
 "use strict";
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-    greet(phrase) {
-        console.log(phrase + ' ' + this.name);
+var _a;
+const e1 = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date(),
+};
+const fetchUserData = {
+    id: 'u1',
+    name: 'John',
+    job: {
+        title: 'hoge',
+        description: 'nanika',
+    },
+};
+console.log((_a = fetchUserData === null || fetchUserData === void 0 ? void 0 : fetchUserData.job) === null || _a === void 0 ? void 0 : _a.title);
+const userInput = '';
+const storeData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
+console.log(storeData);
+function printEmployeeInformation(emp) {
+    console.log(emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
     }
 }
-let user1;
-user1 = new Person('Max');
-user1.greet('Hello Max!');
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log('車を運転中う');
+    }
+}
+class Truck {
+    drive() {
+        console.log('トラックを運転中');
+    }
+    loadCargo(load) {
+        console.log('荷物の重さは' + load + 'kgです。');
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function outputViecleInfo(viecle) {
+    viecle.drive();
+    if (viecle instanceof Truck) {
+        viecle.loadCargo(1000);
+    }
+}
+outputViecleInfo(v1);
+outputViecleInfo(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log('移動速度' + speed);
+}
+moveAnimal({ type: 'bird', flyingSpeed: 500 });
+// const userInputElement = <HTMLInputElement>document.getElementById('message-output');
+const userInputElement = document.getElementById('message-output');
+userInputElement.value = 'こんにちは';
 //# sourceMappingURL=app.js.map
--- udemy_understanding_typescript/src/app.ts
+++ udemy_understanding_typescript/src/app.ts
@@ -1,18 +1,113 @@
-interface Greetable {
-  readonly name: string;
-  outputName?: string;
+type Admin = {
+  name: string;
+  privileges: string[];
+};
 
-  greet(phrase: string): void;
-}
+type Employee = {
+  name: string;
+  startDate: Date;
+};
 
-class Person implements Greetable {
-  constructor(public name: string) {}
+type ElevatedEmploee = Admin & Employee;
 
-  greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+const e1: ElevatedEmploee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+const fetchUserData = {
+  id: 'u1',
+  name: 'John',
+  job: {
+    title: 'hoge',
+    description: 'nanika',
+  },
+};
+
+console.log(fetchUserData?.job?.title);
+
+const userInput = '';
+
+const storeData = userInput ?? 'DEFAULT';
+console.log(storeData);
+
+type UnkownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnkownEmployee) {
+  console.log(emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
   }
 }
 
-let user1: Greetable;
-user1 = new Person('Max');
-user1.greet('Hello Max!');
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log('車を運転中う');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('トラックを運転中');
+  }
+
+  loadCargo(load: number) {
+    console.log('荷物の重さは' + load + 'kgです。');
+  }
+}
+
+type Viecle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function outputViecleInfo(viecle: Viecle) {
+  viecle.drive();
+  if (viecle instanceof Truck) {
+    viecle.loadCargo(1000);
+  }
+}
+
+outputViecleInfo(v1);
+outputViecleInfo(v2);
+
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+
+  switch (animal.type) {
+    case 'bird':
+          speed = animal.flyingSpeed;
+      break;
+      case 'horse':
+          speed = animal.runningSpeed;
+          break;
+  }
+  console.log('移動速度' + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 500})
+
+// const userInputElement = <HTMLInputElement>document.getElementById('message-output');
+const userInputElement = document.getElementById('message-output') as HTMLInputElement;
+
+userInputElement.value = 'こんにちは';
