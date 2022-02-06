a755f91dd8e040f917417924ac04c369034a7219
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2022-02-06 20:21:10 +0900

    udemy学習 TSのインターフェースについての学習

--- udemy_understanding_typescript/dist/app.js
+++ udemy_understanding_typescript/dist/app.js
@@ -1,57 +1,13 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
+class Person {
+    constructor(name) {
         this.name = name;
-        // this.name = n;
     }
-    describe() {
-        console.log(`Department: ${this.id}` + this.name);
-        console.log(`currentYear ${Department.currentYear}`);
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
     }
 }
-Department.currentYear = 23022;
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error('レポートが見つかりません。');
-    }
-    static getInstance() {
-        if (this.instance) {
-            return this.instance;
-        }
-        this.instance = new AccountingDepartment('d2', []);
-        return this.instance;
-    }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error('正しい値を設定して下さい。');
-        }
-        this.addReport(value);
-    }
-    addReport(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    prrintReport() {
-        console.log(this.reports);
-    }
-}
-// const adminsIT = new ITDepartment(['Max', 'John']);
-// console.log(adminsIT);
-// adminsIT.outputAdmins();
-// adminsIT.describe();
-// console.log(accounting.mostRecentReport);
-// console.log((accounting.mostRecentReport = 'hogehoge'));
-// accounting.prrintReport();
-// accounting.describe();
-const accounting = AccountingDepartment.getInstance();
-console.log(accounting);
+let user1;
+user1 = new Person('Max');
+user1.greet('Hello Max!');
 //# sourceMappingURL=app.js.map
--- udemy_understanding_typescript/src/app.ts
+++ udemy_understanding_typescript/src/app.ts
@@ -1,66 +1,18 @@
-class Department {
-  static currentYear = 23022;
+interface Greetable {
+  readonly name: string;
+  outputName?: string;
 
-  constructor(private readonly id: string, public name: string) {
-    // this.name = n;
-  }
-  describe() {
-    console.log(`Department: ${this.id}` + this.name);
-    console.log(`currentYear ${Department.currentYear}`);
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  constructor(public name: string) {}
+
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
   }
 }
 
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  private static instance: AccountingDepartment;
-
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error('レポートが見つかりません。');
-  }
-
-  static getInstance() {
-    if (this.instance) {
-      return this.instance;
-    }
-    this.instance = new AccountingDepartment('d2', []);
-    return this.instance;
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error('正しい値を設定して下さい。');
-    }
-    this.addReport(value);
-  }
-
-  private constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting');
-    this.lastReport = reports[0];
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-  prrintReport() {
-    console.log(this.reports);
-  }
-}
-
-// const adminsIT = new ITDepartment(['Max', 'John']);
-// console.log(adminsIT);
-
-// adminsIT.outputAdmins();
-// adminsIT.describe();
-
-// console.log(accounting.mostRecentReport);
-// console.log((accounting.mostRecentReport = 'hogehoge'));
-// accounting.prrintReport();
-// accounting.describe();
-
-const accounting = AccountingDepartment.getInstance();
-console.log(accounting);
-
+let user1: Greetable;
+user1 = new Person('Max');
+user1.greet('Hello Max!');
