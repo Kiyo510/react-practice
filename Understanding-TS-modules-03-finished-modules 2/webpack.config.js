ba303afcb2e2e75a68b614a3c81d871df9b84440
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2022-02-16 23:05:19 +0900

    udemy学習　webpackの設定など

---
+++ Understanding-TS-modules-03-finished-modules 2/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool : 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                excude: /node_modules/,
+            }
+        ],
+        resolve: {
+            extentions: ['.ts', '.js']
+        }
+    }
+}
