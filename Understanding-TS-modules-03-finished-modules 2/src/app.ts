11d32e551de881eb98338ef5eb7586ac9611227f
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2022-02-19 23:59:23 +0900

    udemy学習

--- Understanding-TS-modules-03-finished-modules 2/src/app.ts
+++ Understanding-TS-modules-03-finished-modules 2/src/app.ts
@@ -1,6 +1,2 @@
-import { ProjectInput } from './components/project-input.js';
-import { ProjectList } from './components/project-list.js';
+import _ from 'lodash';
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
