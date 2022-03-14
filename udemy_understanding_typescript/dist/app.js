41d0e7f62cd6c5848bb867478eb2ea25667f3a96
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2022-03-14 23:02:39 +0900

    Udemy 学習

--- udemy_understanding_typescript/dist/app.js
+++ udemy_understanding_typescript/dist/app.js
@@ -1,66 +1,44 @@
 "use strict";
-var _a;
-const e1 = {
-    name: 'Max',
-    privileges: ['create-server'],
-    startDate: new Date(),
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-const fetchUserData = {
-    id: 'u1',
-    name: 'John',
-    job: {
-        title: 'hoge',
-        description: 'nanika',
-    },
-};
-console.log((_a = fetchUserData === null || fetchUserData === void 0 ? void 0 : fetchUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-const userInput = '';
-const storeData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
-console.log(storeData);
-function printEmployeeInformation(emp) {
-    console.log(emp.name);
-    if ('privileges' in emp) {
-        console.log('Privileges: ' + emp.privileges);
+function WithTemplate(templete, hookId) {
+    return function (_) {
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+            hookEl.innerHTML = templete;
+        }
+    };
+}
+// @WithTemplate('<h1>Personオブジェクト<h1>', 'app');
+function Log(target, propertyName) {
+    console.log('Property デコレータ');
+    console.log(target, propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('正の数でお願いします。');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+    getTitle() {
+        return this.title;
     }
 }
-printEmployeeInformation(e1);
-class Car {
-    drive() {
-        console.log('車を運転中う');
-    }
-}
-class Truck {
-    drive() {
-        console.log('トラックを運転中');
-    }
-    loadCargo(load) {
-        console.log('荷物の重さは' + load + 'kgです。');
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function outputViecleInfo(viecle) {
-    viecle.drive();
-    if (viecle instanceof Truck) {
-        viecle.loadCargo(1000);
-    }
-}
-outputViecleInfo(v1);
-outputViecleInfo(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log('移動速度' + speed);
-}
-moveAnimal({ type: 'bird', flyingSpeed: 500 });
-// const userInputElement = <HTMLInputElement>document.getElementById('message-output');
-const userInputElement = document.getElementById('message-output');
-userInputElement.value = 'こんにちは';
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
 //# sourceMappingURL=app.js.map
