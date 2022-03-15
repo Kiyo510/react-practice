61e35267fe380bb6ea51e0179791bdbb0073b329
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2022-03-15 22:50:24 +0900

    Udemy学習

--- Understanding-TS-prj-libs-01-starting-setup/src/app.ts
+++ Understanding-TS-prj-libs-01-starting-setup/src/app.ts
@@ -1,1 +1,41 @@
-// ここにコードを書きます
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+const GOOGLE_API_KEY = "hogehgoe";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://url/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("座標を取得できませんでした");
+      }
+        const corrdinates = response.data.results[0].geometry.location;
+        
+          const map = new google.maps.Map(document.getElementById("map")!, {
+            center: corrdinates,
+            zoom: 16,
+          });
+          console.log(map);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+
+}
+
+form.addEventListener("submit", searchAddressHandler);
